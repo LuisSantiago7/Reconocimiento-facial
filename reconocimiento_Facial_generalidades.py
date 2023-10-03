eeb2dba733ad7f251b447df63296a5d4abda1573
Luis Angel Eleazar Santiago <108829227+LuisSantiago7@users.noreply.github.com>
2023-10-03 14:14:54 -0600

    Delete reconocimiento_Facial_generalidades.py

--- reconocimiento_Facial_generalidades.py
+++
@@ -1,8 +0,0 @@
-'''Para este pryecto necesitaremos descargar algunas bibliotecas e importarlas'''
-
-#1.- visual studio con desarrollo C++ en desktop
-#2.- CMake
-#3.- Dlib
-#4.- face-recognition
-#5.- opencv-python
-#6.- numpy
